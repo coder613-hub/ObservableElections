b14216e75764e59262c04c59b2ac4aac8c00b7f2
coder613-hub <96893407+coder613-hub@users.noreply.github.com>
2024-01-24 22:42:52 -0500

    initial commit

--- IElectoralVoteStrategy.java
+++ IElectoralVoteStrategy.java
@@ -1,8 +1,7 @@
 package assignment5;
 
 import java.util.ArrayList;
-//Sara Nordlicht
-//Penina Rockove
+
 public interface IElectoralVoteStrategy {
 	public ElectoralVote getElectoralStrategy(ArrayList<State> states);
 	public String getStrategyName();
