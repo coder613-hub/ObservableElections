16890c5c646354edf87e7f240c22611b7d47dd68
coder613-hub <96893407+coder613-hub@users.noreply.github.com>
2024-01-24 22:42:25 -0500

    initial commit

--- HonestObserver.java
+++ HonestObserver.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.Observable;
 
 public class HonestObserver extends ReportStatistics{
-	//Sara Nordlicht
-	//Penina Rockove
+	
 	public HonestObserver(Observable o, ArrayList<State> states) {
 		super(o, states);
 		super.setPopStrategy(new HonestPop());
