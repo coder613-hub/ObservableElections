b2b82d45385ae57d6ecbe5ce735fd33a19f139b6
coder613-hub <96893407+coder613-hub@users.noreply.github.com>
2024-01-24 22:43:06 -0500

    initial commit

--- IPopularVoteStrategy.java
+++ IPopularVoteStrategy.java
@@ -1,8 +1,7 @@
 package assignment5;
 
 import java.util.ArrayList;
-//Sara Nordlicht
-//Penina Rockove
+
 public interface IPopularVoteStrategy {
 	public PopularVote getPopularStrategy(ArrayList<State> states);
 	public String getStrategyName();
