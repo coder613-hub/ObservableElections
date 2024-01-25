c3bd4704bb65897ea7af1ae392aa1b85e59b35bc
coder613-hub <96893407+coder613-hub@users.noreply.github.com>
2024-01-24 22:40:30 -0500

    initial commit

--- DemFavoredPopObserver.java
+++ DemFavoredPopObserver.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.Observable;
 
 public class DemFavoredPopObserver extends ReportStatistics{
-	//Sara Nordlicht
-	//Penina Rockove
+	
 	public DemFavoredPopObserver(Observable o, ArrayList<State> states) {
 		super(o, states);
 		super.setPopStrategy(new IgnoreHighestRepState());
