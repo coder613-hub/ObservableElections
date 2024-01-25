976d2ecd4183e90317757c6a7886d896ccd8bc26
coder613-hub <96893407+coder613-hub@users.noreply.github.com>
2024-01-24 22:41:33 -0500

    initial commit

--- ElectionTooCloseToCallFavorsRep.java
+++ ElectionTooCloseToCallFavorsRep.java
@@ -2,16 +2,7 @@ package assignment5;
 
 import java.util.ArrayList;
 
-//Sara Nordlicht
-//Penina Rockove
 public class ElectionTooCloseToCallFavorsRep implements IElectoralVoteStrategy {
-	// There is a second republican favoring strategy that
-	// considers the state where the democrat has the smallest lead to be "too close
-	// to call"
-	// and splits the electoral votes
-	// giving half to the democrat and half to the republican
-	// (and in the event of an odd number it gives the extra vote to the republican
-	// of course)
 	@Override
 	public ElectoralVote getElectoralStrategy(ArrayList<State> states) {
 		State smallestDemLead = getStateWithSmallestDemLead(states);
